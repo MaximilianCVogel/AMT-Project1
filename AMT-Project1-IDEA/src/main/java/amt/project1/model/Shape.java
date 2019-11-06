7ce6007d398ba907ceac748c8204f2c73e508535
Maximilian Vogel <maximilian.c.vogel@gmail.com>
2019-11-06 05:43:08 +0100

    Initial commit : project structure

---
+++ AMT-Project1-IDEA/src/main/java/amt/project1/model/Shape.java
@@ -0,0 +1,21 @@
+package amt.project1.model;
+
+public class Shape {
+
+    private String name;
+    private String color;
+
+    public Shape(String name, String citation) {
+        this.name = name;
+        this.color = citation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+}
